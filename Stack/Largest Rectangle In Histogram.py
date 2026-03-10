839fb168feff9c9005d4177ce69d9ba93897fbd7
osamasrour <oasmasror@gmail.com>
2026-03-10 05:28:49 +0200

    Latest progress in enhancing 'Largest Rectangle in Histogram'

--- Stack/Largest Rectangle In Histogram.py
+++ Stack/Largest Rectangle In Histogram.py
@@ -3,22 +3,40 @@
 class Solution:
     def largestRectangleArea(self, heights: list[int]) -> int:
         n = len(heights)
-
-        largest_area = -1
+        largest_area = 0
+        stack: list[tuple[int, int]] = []
         for i in range(n):
-            current_width = 1
-
-            prev = i - 1
-            while prev >= 0 and heights[prev] >= heights[i]:
-                current_width+=1
-                prev-=1
-
-            _next = i + 1
-            while _next < n and heights[_next] >= heights[i]:
-                current_width += 1
-                _next+=1
-
-            area = current_width * heights[i]
-            largest_area = max(largest_area, area)
-
+            skiped = 0
+            while stack and heights[i] < stack[-1][1]:
+                poped_idx, poped_value = stack.pop()
+                poped_area = poped_value * (i - poped_idx)
+                skiped+=1
+                largest_area = max(poped_area, largest_area)
+            if stack:
+                stack.append((i - skiped, heights[i]))
+            else:
+                stack.append((0, heights[i]))
+        for i in range(len(stack)):
+            poped_idx, poped_value = stack.pop()
+            poped_area = poped_value * (n - poped_idx)
+            largest_area = max(poped_area, largest_area)       
         return largest_area
+
+
+obj = Solution()
+heights = [7,1,7,2,2,4]
+print(obj.largestRectangleArea(heights) == 8)
+heights = [1,3,7]
+print(obj.largestRectangleArea(heights) == 7)
+heights=[3,6,5,7,4,8,1,0]
+# index=[0,1,2,3,4,5,6,7]
+"""
+(3, 0) x 18
+(6, 1) x 6
+(5, 1) x 18
+(7, 3) x 7
+(4, 2) x 16
+(8, 5) x 8
+(1, 3)
+"""
+print(obj.largestRectangleArea(heights) == 20)
