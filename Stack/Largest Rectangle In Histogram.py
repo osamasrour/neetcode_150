d02e5cff9a18cf5372c83125620be24271106395
osamasrour <oasmasror@gmail.com>
2026-03-10 02:08:08 +0200

    solve Largest Rectangle In Histogram problem

---
+++ Stack/Largest Rectangle In Histogram.py
@@ -0,0 +1,24 @@
+# Largest Rectangle In Histogram: https://neetcode.io/problems/largest-rectangle-in-histogram/question?list=neetcode150
+
+class Solution:
+    def largestRectangleArea(self, heights: list[int]) -> int:
+        n = len(heights)
+
+        largest_area = -1
+        for i in range(n):
+            current_width = 1
+
+            prev = i - 1
+            while prev >= 0 and heights[prev] >= heights[i]:
+                current_width+=1
+                prev-=1
+
+            _next = i + 1
+            while _next < n and heights[_next] >= heights[i]:
+                current_width += 1
+                _next+=1
+
+            area = current_width * heights[i]
+            largest_area = max(largest_area, area)
+
+        return largest_area
