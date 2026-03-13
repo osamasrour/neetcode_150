93aed03d5f15d494559cf9b8d816e0e6dc3ffd39
osamasrour <oasmasror@gmail.com>
2026-03-13 06:53:04 +0200

    final solution for Search a 2D Matrix.

--- Binary Search/Search a 2D Matrix.py
+++ Binary Search/Search a 2D Matrix.py
@@ -17,29 +17,3 @@ class Solution:
             else:
                 return True
         return False
-
-obj = Solution()
-
-matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]]
-target = 10
-print(obj.searchMatrix(matrix, target) == True)
-
-matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]]
-target = 15
-print(obj.searchMatrix(matrix, target) == False)
-
-matrix=[[1,3,5,7],[10,11,16,20],[23,30,34,60]]
-target=13
-print(obj.searchMatrix(matrix, target) == False)
-
-matrix=[[1,3]]
-target=1
-print(obj.searchMatrix(matrix, target) == True)
-
-matrix=[[1]]
-target=0
-print(obj.searchMatrix(matrix, target) == False)
-
-matrix=[[1,1]]
-target=2
-print(obj.searchMatrix(matrix, target) == False)
