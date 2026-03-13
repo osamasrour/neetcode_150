fca3b7d6c2d97e574041123bbcdc86064cef9842
osamasrour <oasmasror@gmail.com>
2026-03-13 06:39:52 +0200

    better solution for Search a 2D Matrix.

--- Binary Search/Search a 2D Matrix.py
+++ Binary Search/Search a 2D Matrix.py
@@ -1,16 +1,15 @@
 # Search a 2D Matrix: https://neetcode.io/problems/search-2d-matrix/question?list=neetcode150
-
+from math import ceil
 class Solution:
     def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
         n = len(matrix[0]) # number of elements in one row
         m = len(matrix) # number of rows
         length = m * n
-
-        l, r = 0, length -1
-
+        l, r = 1, length
         while l <= r:
-            middle = (l + r) // 2
-            middleM, middleN = middle // n, middle % n # someting wrong here in the last element of every list
+            middle  = ((l + r) // 2)
+            middleM = ceil(middle / n) -1
+            middleN = (middle % n) -1
             if matrix[middleM][middleN] > target:
                 r = middle - 1
             elif matrix[middleM][middleN] < target:
@@ -27,4 +26,20 @@ print(obj.searchMatrix(matrix, target) == True)
 
 matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]]
 target = 15
+print(obj.searchMatrix(matrix, target) == False)
+
+matrix=[[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+target=13
+print(obj.searchMatrix(matrix, target) == False)
+
+matrix=[[1,3]]
+target=1
+print(obj.searchMatrix(matrix, target) == True)
+
+matrix=[[1]]
+target=0
+print(obj.searchMatrix(matrix, target) == False)
+
+matrix=[[1,1]]
+target=2
 print(obj.searchMatrix(matrix, target) == False)
