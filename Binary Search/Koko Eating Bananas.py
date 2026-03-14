b02ece870deacdfa3b161900996151388cc57c16
osamasrour <oasmasror@gmail.com>
2026-03-14 07:11:06 +0200

    Final Solution for Koko Eating Bananas passed 25/25 test cases

--- Binary Search/Koko Eating Bananas.py
+++ Binary Search/Koko Eating Bananas.py
@@ -2,73 +2,25 @@
 
 from math import ceil
 class Solution:
-    def eatTime(self, k, piles):
+    def isValidEatTime(self, k, piles, h):
         totalTime = 0
         n = len(piles)
         for i in range(n):
             pile_time = (ceil(piles[i] / k))
             totalTime += pile_time
-        return totalTime
+        return totalTime <= h
 
 
     def minEatingSpeed(self, piles: list[int], h: int) -> int:
         n = len(piles)
-        max_val = 0
-        for e in piles:
-            max_val = max(max_val, e)
+        max_val = max(piles)
         l, r = 1, max_val
-        # print([x for x in range(l, max_val + 1)])
-        max_k_time = 0
-        k_result = -1
+        k = 0
         while l <= r:
-            k = (l + r) // 2
-            k_time = self.eatTime(k, piles)
-            # print(f"l = {l}, r = {r}")
-            if k_time > h:
-                l = k + 1
-            elif k_time < h:
-                r = k - 1
-                if k_time > max_k_time:
-                    max_k_time = k_time
-                    k_result = k
-            
+            m = ((l + r) // 2)
+            if self.isValidEatTime(m, piles, h):
+                k = m
+                r = m - 1
             else:
-                if k_time > max_k_time:
-                    max_k_time = k_time
-                    k_result = k
-                break
-            # print(f"k = {k}, k_time = {k_time}")
-            # print(f"l = {l}, r = {r}")
-            # input()
-        return k_result
-
-
-
-
-
-
-obj = Solution()
-
-piles = [1,4,3,2]
-h = 9
-print(obj.minEatingSpeed(piles, h) == 2)
-
-piles = [25,10,23,4]
-h = 4
-print(obj.minEatingSpeed(piles, h) == 25)
-
-piles=[3,6,7,11]
-h = 8
-print(obj.minEatingSpeed(piles, h) == 4)
-
-piles=[312884470]
-h=312884469
-print(obj.minEatingSpeed(piles, h) == 2)
-
-piles=[332484035,524908576,855865114,632922376,222257295,690155293,112677673,679580077,337406589,290818316,877337160,901728858,679284947,688210097,692137887,718203285,629455728,941802184]
-h=823855818
-print(obj.minEatingSpeed(piles, h) == 14)
-
-piles=[1,1,1,999999999]
-h=10
-print(obj.minEatingSpeed(piles, h) == 142857143)
+                l = m + 1
+        return k
