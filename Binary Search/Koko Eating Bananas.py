9228e904eb9916fdff1032b7983500dbbc720de7
osamasrour <oasmasror@gmail.com>
2026-03-14 06:41:14 +0200

    Better Solution passes 21/25 tests

--- Binary Search/Koko Eating Bananas.py
+++ Binary Search/Koko Eating Bananas.py
@@ -13,20 +13,35 @@ class Solution:
 
     def minEatingSpeed(self, piles: list[int], h: int) -> int:
         n = len(piles)
-        max_eat = 0
         max_val = 0
-        min_val = float("inf")
-        for i in piles:
-            max_val = max(max_val, i)
-            min_val = min(min_val, i)
+        for e in piles:
+            max_val = max(max_val, e)
+        l, r = 1, max_val
+        # print([x for x in range(l, max_val + 1)])
+        max_k_time = 0
+        k_result = -1
+        while l <= r:
+            k = (l + r) // 2
+            k_time = self.eatTime(k, piles)
+            # print(f"l = {l}, r = {r}")
+            if k_time > h:
+                l = k + 1
+            elif k_time < h:
+                r = k - 1
+                if k_time > max_k_time:
+                    max_k_time = k_time
+                    k_result = k
+            
+            else:
+                if k_time > max_k_time:
+                    max_k_time = k_time
+                    k_result = k
+                break
+            # print(f"k = {k}, k_time = {k_time}")
+            # print(f"l = {l}, r = {r}")
+            # input()
+        return k_result
 
-        pair = dict() # eatTime: k
-        for i in range(max_val, min_val - 1, -1):
-            x = self.eatTime(i, piles)
-            pair[x] = i
-            if x <= h:
-                max_eat = max(max_eat, x)
-        return pair[max_eat]
 
 
 
@@ -45,7 +60,7 @@ print(obj.minEatingSpeed(piles, h) == 25)
 piles=[3,6,7,11]
 h = 8
 print(obj.minEatingSpeed(piles, h) == 4)
-exit()
+
 piles=[312884470]
 h=312884469
 print(obj.minEatingSpeed(piles, h) == 2)
@@ -53,3 +68,7 @@ print(obj.minEatingSpeed(piles, h) == 2)
 piles=[332484035,524908576,855865114,632922376,222257295,690155293,112677673,679580077,337406589,290818316,877337160,901728858,679284947,688210097,692137887,718203285,629455728,941802184]
 h=823855818
 print(obj.minEatingSpeed(piles, h) == 14)
+
+piles=[1,1,1,999999999]
+h=10
+print(obj.minEatingSpeed(piles, h) == 142857143)
