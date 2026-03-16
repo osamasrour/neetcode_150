a6ea84cd340fb0abd0c6dfbf42886e309a9edf88
osamasrour <oasmasror@gmail.com>
2026-03-16 07:48:19 +0200

    better solution for Search in Rotated Sorted Array passed 35/35 test cases

--- Binary Search/Search in Rotated Sorted Array.py
+++ Binary Search/Search in Rotated Sorted Array.py
@@ -8,18 +8,18 @@ class Solution:
         l, r = 0, n-1
         if nums[l] == target: return l
         if nums[r] == target: return r
-        while l < r:
+        while l <= r:
             m = l +((r - l) // 2)
             if nums[r] < nums[l]: # handle the un sorted part
                 if target < nums[l]:
                     # the target in the R part
                     if nums[m] > nums[l]:
                         l = m + 1
-                    else:  #nums[m] < nums[l]
+                    else:  # nums[m] < nums[l]
                         if nums[m] < target:
-                            l = m + 1
+                            l = m
                         elif nums[m] > target:
-                            r = m - 1
+                            r = m
                         else:
                             return m
                 elif target > nums[l]:
@@ -30,12 +30,11 @@ class Solution:
                         if nums[m] < target:
                             l = m + 1
                         elif nums[m] > target:
-                            r = m - 1
+                            r = m + 1
                         else:
                             return m
                 else:
                     return l
-
             else:
                 if nums[m] < target:
                     l = m + 1
@@ -47,47 +46,3 @@ class Solution:
             return m
         else:
             return -1
-
-# when to move r by m or m -1
-# nums[m] > target and target > nums[r]
-
-# [6,0,1,2,3,4,5]
-
-
-obj = Solution()
-
-nums = [3,4,5,6,1,2]
-target = 1
-print(obj.search(nums, target) == 4)
-
-nums = [3,5,6,0,1,2]
-target = 4
-print(obj.search(nums, target) == -1)
-
-nums = [0,1,2,3,5,6]
-target = 6
-print(obj.search(nums, target) == 5)
-
-nums = [0,1,2,3,5,6]
-target = 0
-print(obj.search(nums, target) == 0)
-
-nums = [0,1,2,3,5,6]
-target = 2
-print(obj.search(nums, target) == 2)
-
-nums = [0,1,2,3,5,6]
-target = 4
-print(obj.search(nums, target) == -1)
-
-nums=[1,3]
-target=3
-print(obj.search(nums, target) == 1)
-
-nums=[3,1]
-target=1
-print(obj.search(nums, target) == 1)
-
-nums=[5,1,2,3,4]
-target=1
-print(obj.search(nums, target) == 1)
