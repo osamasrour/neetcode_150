6a01f491579d3d7becdf123a2b4cc04d7743bfea
osamasrour <oasmasror@gmail.com>
2026-03-19 07:07:00 +0200

    enhancement(#2) Solve Trapping Rain Water using an O(n) solution.

--- Two Pointers/Trapping Rain Water.py
+++ Two Pointers/Trapping Rain Water.py
@@ -1,29 +1,29 @@
 # Trapping Rain Water: https://neetcode.io/problems/trapping-rain-water/question?list=neetcode150
-# TODO(#2): make it O(n)
+
 class Solution:
     def trap(self, height: list[int]) -> int:
         n = len(height)
-        if n < 2: return 0
-        start_idx = 0
-        end_idx = n - 1
+        full_area = 0
+        max_l = 0
+        l_wall = [0]
         for i in range(n):
-        	if height[start_idx] < height[start_idx + 1]:
-        		start_idx += 1
-        	if height[end_idx] < height[end_idx -1]:
-        		end_idx -= 1
-        max_area = 0
+            if height[i] >= max_l:
+                max_l = height[i]
+                l_wall.append(max_l)
+            else:
+                l_wall.append(max_l)
+        
+        max_r = 0
+        r_wall = [0]
+        for i in range(n - 1, -1, -1):
+            if height[i] >= max_r:
+                max_r = height[i]
+                r_wall.append(max_r)
+            else:
+                r_wall.append(max_r)
 
-        max_val = 0
-        for i in range(start_idx, end_idx + 1):
-        	max_val = max(height[i], max_val)
-        for h in range(max_val, 0, -1):
-        	arr_of_hs = []
-        	for i in range(n):
-        		if height[i] >= h:
-        			arr_of_hs.append(i)
-        	arr_len = len(arr_of_hs)
-        	for j in range(arr_len - 1):
-        		k = j + 1
-        		area = (arr_of_hs[k] - arr_of_hs[j] - 1)
-        		max_area += area
-        return max_area
+        for i in range(n):
+            area = min(l_wall[i], r_wall[n - i - 1]) - height[i]
+            full_area += max(0, area)
+
+        return full_area
