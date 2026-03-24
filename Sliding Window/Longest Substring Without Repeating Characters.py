3c8f4f1b2e920e9087a547cf87d70af17dbe4347
osamasrour <oasmasror@gmail.com>
2026-03-25 00:22:21 +0200

    Better solution for Longest Substring Without Repeating Characters passed 30/30 test cases.

--- Sliding Window/Longest Substring Without Repeating Characters.py
+++ Sliding Window/Longest Substring Without Repeating Characters.py
@@ -3,35 +3,23 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         n = len(s)
-        unique = set()
-
+        unique = dict()
         lswr = 0
+        if n == 1:
+            return 1
         for i in range(n):
-            if not unique:
-                unique.add(s[i])
-                lswr = 1
-                continue
-            if s[i] in unique:
-                lswr = max(len(unique), lswr)
-                unique.clear()
-                unique.add(s[i])
-
+            if not unique.get(s[i]) : # the s[i] not in the unique hashmap
+                unique[s[i]] = i
+                lswr = max(lswr, len(unique))
             else:
-                unique.add(s[i])
-        lswr = max(len(unique), lswr)
+                j = unique[s[i]]
+                delete_keys = []
+                for k, v in unique.items():
+                    if v <= j:
+                        delete_keys.append(k)
+                for key in delete_keys:
+                    del unique[key]
+                unique[s[i]] = i
+                lswr = max(lswr, len(unique))
+        lswr = max(lswr, len(unique))
         return lswr
-
-
-obj = Solution()
-
-s = "zxyzxyz"
-print(obj.lengthOfLongestSubstring(s) == 3)
-
-s = "xxxx"
-print(obj.lengthOfLongestSubstring(s) == 1)
-
-s=" "
-print(obj.lengthOfLongestSubstring(s) == 1)
-
-s="dvdf"
-print(obj.lengthOfLongestSubstring(s) == 3)
