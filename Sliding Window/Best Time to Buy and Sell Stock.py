d825fef4aa1bb9edb4baea44cc6b2c68dca3d90d
osamasrour <oasmasror@gmail.com>
2026-03-24 23:00:08 +0200

    Better solution for Best Time to Buy and Sell Stock, Time complexity = O(n)

--- Sliding Window/Best Time to Buy and Sell Stock.py
+++ Sliding Window/Best Time to Buy and Sell Stock.py
@@ -2,20 +2,11 @@
 
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        # naive approach 26ms
         n = len(prices)
+        buy = float("inf")
         profit = 0
         for i in range(n):
-            buy = prices[i]
-            for j in range(i + 1, n):
-                sell = prices[j]
-                profit = max(profit, sell - buy)
-
+            if buy > prices[i]:
+                buy = prices[i]
+            profit = max(profit, prices[i] - buy)
         return profit
-obj = Solution()
-
-prices = [10,1,5,6,7,1]
-print(obj.maxProfit(prices) == 6)
-
-prices = [10,8,7,5,2]
-print(obj.maxProfit(prices) == 0)
