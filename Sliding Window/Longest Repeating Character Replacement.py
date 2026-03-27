cbed86d9d02aa89a9908d7391980e6ecf1279cb9
osamasrour <oasmasror@gmail.com>
2026-03-27 05:27:46 +0200

    Better solution for 'Longest Repeating Character Replacement' passed 26 / 26 test cases.

--- Sliding Window/Longest Repeating Character Replacement.py
+++ Sliding Window/Longest Repeating Character Replacement.py
@@ -3,51 +3,16 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         n = len(s)
-        p1=p2=0
-        consume_k = 0
-
-        lrcr = 0;
-        curr_c = s[0]
-        for i in range(1, n):
-            c = s[i]
-            if c == curr_c:
-                p2 += 1
-                print(f"p2 = {p2}")
-            else:
-                if (k - consume_k) > 0:
-                    consume_k+=1
-                else:
-                    p1 = p2 + 1
-                    p2 = p1
-                    if p1 + 1 >= n:
-                        break
-                    i = p1
-                    curr_c = s[i]
-                    print(f"curr_c = {curr_c}")
-                    consume_k = 0
-            lrcr = max(lrcr, p2 - p1 + consume_k + 1)
-        print(f"lrcr = {lrcr}")
+        l=0
+        lrcr = 0
+        windowSize = 0
+        counter = dict()
+        for r in range(n):
+            counter[s[r]] = counter.get(s[r], 0) + 1
+            mostFreq=max(counter.values())
+            windowSize = r - l + 1
+            if windowSize - mostFreq > k:
+                counter[s[l]]-=1
+                l += 1
+            lrcr = max(lrcr, r-l+1)
         return lrcr
-
-
-obj = Solution()
-
-s = "XYYX"
-k = 2
-print(obj.characterReplacement(s, k) == 4)
-
-s = "AAABABB"
-k = 1
-print(obj.characterReplacement(s, k) == 5)
-
-s="AAAB"
-k=0
-print(obj.characterReplacement(s, k) == 3)
-
-s="ABAA"
-k=0
-print(obj.characterReplacement(s, k) == 2)
-
-s="ABBB"
-k=2
-print(obj.characterReplacement(s, k) == 4)
