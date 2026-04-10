2e4d918ed6f77802afbaee254abaf989a4d3d867
osamasrour <oasmasror@gmail.com>
2026-04-10 16:19:07 +0200

    Solving 'Permutation in String' passed 31/31 test cases.

--- Sliding Window/Permutation in String.py
+++ Sliding Window/Permutation in String.py
@@ -1,41 +1,30 @@
 # Permutation in String : https://neetcode.io/problems/permutation-string/question?list=neetcode150
 
 class Solution:
+
+    def makeHashMap(self, string: str) -> dict[str, int] :
+        d = dict()
+        for i in range(len(string)):
+            if d.get(string[i]) != None:
+                d[string[i]] += 1
+            else:
+                d[string[i]] = 1
+        return d
+
+
     def checkInclusion(self, s1: str, s2: str) -> bool:
         n1= len(s1)
         n2= len(s2)
 
-        # check if s1 is permutation of s2
-        h1 = set(s1)
-        for l in range(0, n2 - n1 + 1):
-            counter = 0
-            container = set()
-            for i in range(n1):
-                if s2[l + i] in h1 and s2[l + i] not in container:
-                    counter += 1
-                    container.add(s2[l+i])
-                    print("s2[l] = `", s2[l], "`, s2[l+i] = `", s2[l+i], "`, counter = ", counter)
-
-            if counter == n1: return True
+        hm1 = self.makeHashMap(s1)
+        l = 0
+        while l < n2 - n1 + 1:
+            match_counter = 0
+            hm2Window = self.makeHashMap(s2[l: l + n1])
+            for k, v in hm2Window.items():
+                if hm1.get(k) == v:
+                    match_counter += 1
+            if match_counter == len(hm1):
+                return True
+            l+= 1
         return False
-        
-
-
-
-s1 = "abc"
-s2 = "lecabee"
-print(Solution().checkInclusion(s1, s2) == True)
-
-s1 = "abc"
-s2 = "lecaabee"
-print(Solution().checkInclusion(s1, s2) == False)
-
-s1="adc"
-s2="dcda"
-print(Solution().checkInclusion(s1, s2) == True)
-
-s1="trinitrophenylmethylnitramine"
-s2="dinitrophenylhydrazinetrinitrophenylmethylnitramine"
-print(Solution().checkInclusion(s1, s2) == True)
-
-
