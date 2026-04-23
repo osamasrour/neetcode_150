20cb77f978b1d9e80166eabaf701aef150e41ae8
osamasrour <oasmasror@gmail.com>
2026-04-23 05:09:01 +0200

    LRU Cache: passed 7/22

--- Linked List/LRU Cache.py
+++ Linked List/LRU Cache.py
@@ -1,8 +1,9 @@
 # LRU Cache: https://neetcode.io/problems/lru-cache/question?list=neetcode150
 
 class Node:
-    def __init__(self: Node, val: int):
+    def __init__(self, key: int, val: int):
         self.val = val
+        self.key = key
         self.prev = None
         self.next = None
 
@@ -12,73 +13,125 @@ class Dll:
         self.last = None
         self.count = 0
 
-    def addFront(self, val):
+    def addFront(self, key, val):
         if self.front == None:
-            assert(self.last == None and self.count == 0)
-            node = Node(val)
+            assert self.last == None and self.count == 0
+            node = Node(key, val)
             node.prev = node
             node.next = node
             self.last = self.front = node
             self.count = 1
         else:
-            node = Node(val)
+            node = Node(key, val)
             self.front.prev = node
             node.next = self.front
             self.front = node
-            self.count+=1
+            self.count += 1
 
-    def addLast(self, val):
+    def addLast(self, key, val):
         if self.last == None:
-            assert(self.front == None and self.count == 0)
-            node = Node(val)
+            assert self.front == None and self.count == 0
+            node = Node(key, val)
             node.prev = node
             node.next = node
             self.last = self.front = node
             self.count = 1
         else:
-            node = Node(val)
+            node = Node(key, val)
             self.last.next = node
             node.prev = self.last
             self.last = node
-            self.count+=1
+            self.count += 1
+
+    def remove(self, node: Node):
+        if self.front == node:
+            self.front = self.front.next
+            self.front.prev = None
+            self.count-=1
+        elif node == self.last:
+            self.last = self.last.prev
+            self.last.next = None
+            self.count -= 1
+        else:
+            curr = self.front.next
+            while curr != self.last:
+                if node == curr:
+                    prev = curr.prev
+                    _next = curr.next
+                    prev.next = _next
+                    _next.prev = prev
+                    curr.prev = None
+                    curr.next = None
+                    self.count-=1
+                    break
+                else:
+                    curr = curr.next
+
 
     def pop(self) -> Node:
         lastNode = self.last
         self.last = self.last.prev
         self.last.next = None
-        lastNode.prev = None
-        lastNode.next = None
         self.count -= 1
         return lastNode
 
 
 class LRUCache:
-
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.hm: dict[int: Node] = dict()
+        self.hm: dict[int:Node] = dict()
         self.dll = Dll()
-
 
     def get(self, key: int) -> int:
         ret = self.hm.get(key, -1)
+
         if ret != -1:
+            node = ret
+            self.dll.remove(node)
+            self.dll.addFront(key, node.val)
+            self.hm[key] = self.dll.front
             ret = ret.val
         return ret
 
     def put(self, key: int, value: int) -> None:
+        if self.hm.get(key) != None:
+            self.hm[key].val = value
+            return
         if self.dll.count + 1 > self.cap:
-            rm = []
-            for k, v in self.hm.items():
-                if self.hm[k] == self.dll.last:
-                    rm.append(k)
-
-            assert(len(rm) == 1)
-            del self.hm[rm[0]]
+            del self.hm[self.dll.last.key]
             self.dll.pop()
 
         assert(self.dll.count < self.cap)
-
-        self.dll.addFront(value)
+        self.dll.addFront(key, value)
         self.hm[key] = self.dll.front
+        assert len(self.hm) == self.dll.count
 
+# ["LRUCache", [2], "put", [1, 10],
+  # "get", [1], "put", [2, 20], "put", [3, 30], "get", [2], "get", [1]]
+
+# [null,null,10,null,null,20,-1]
+
+cache = LRUCache(2)
+print(cache.put(1,10) == None)
+print(cache.get(1) == 10)
+print(cache.put(2,20) == None)
+print(cache.put(3,30) == None)
+print(cache.get(2) == 20)
+print(cache.get(1) == -1)
+
+# ["LRUCache", [2], "put", [1, 1], "put", [2, 2], "get", [1], "put", [3, 3], 
+# "get", [2], "put", [4, 4], "get", [1], "get", [3], "get", [4]]
+
+# [null,null,null,1,null,-1,null,-1,3,4]
+
+cache = LRUCache(2)
+print(cache.put(1, 1) == None)
+print(cache.put(2, 2) == None)
+print(cache.get(1) == 1)
+print(cache.put(3, 3) == None)
+print(cache.get(2) == -1) # should be -1
+
+print(cache.put(4, 4) == None)
+print(cache.get(1) == -1)
+print(cache.get(3) == 3)
+print(cache.get(4) == 4)
