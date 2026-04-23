4dc4f3cdcf83b6e53f4a835fc38e91e130bfca1e
osamasrour <oasmasror@gmail.com>
2026-04-23 03:05:35 +0200

    Introduce Doubly Linked List Structure

--- Linked List/LRU Cache.py
+++ Linked List/LRU Cache.py
@@ -1,38 +1,65 @@
 # LRU Cache: https://neetcode.io/problems/lru-cache/question?list=neetcode150
 
-from collections import deque
+class Node:
+    def __init__(self: Node, val: int):
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class Dll:
+    def __init__(self):
+        self.front = None
+        self.last = None
+        self.count = 0
+
+    def addFront(self, val):
+        if self.front == None:
+            assert(self.last == None and self.count == 0)
+            node = Node(val)
+            node.prev = node
+            node.next = node
+            self.last = self.front = node
+            self.count = 1
+        else:
+            node = Node(val)
+            self.front.prev = node
+            node.next = self.front
+            self.front = node
+            self.count+=1
+
+    def addLast(self, val):
+        if self.last == None:
+            assert(self.front == None and self.count == 0)
+            node = Node(val)
+            node.prev = node
+            node.next = node
+            self.last = self.front = node
+            self.count = 1
+        else:
+            node = Node(val)
+            self.last.next = node
+            node.prev = self.last
+            self.last = node
+            self.count+=1
+
+    def pop(self) -> Node:
+        lastNode = self.last
+        lastNode.prev = None
+        lastNode.next = None
+        self.last = self.last.prev
+        self.last.next = None
+        self.count -= 1
+        return lastNode
+
+
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.count = 0
-        self.hm: dict[int: int] = dict()
-        self.dll: deque = deque()
+        pass
 
     def get(self, key: int) -> int:
-        value = self.hm.get(key)
-        if value == None: return -1
-
-        if self.dll.index(value) != 0:
-            self.dll.remove(value)
-            self.dll.append(value)
-            self.hm[key] = self.dll[0]
-
-        return value
+        pass
 
     def put(self, key: int, value: int) -> None:
-        if self.count + 1 > self.cap:
-            val = self.dll.popleft()
-            once = 0
-            for k, v in self.hm.items():
-                if v == val:
-                    del self.hm[k]
-                    once+= 1
-            assert(once == 1, "should happend only once")
-
-        self.hm[key] = value
-        self.dll.append(value)
-        self.count+=1
-
-
-        
+        pass  
