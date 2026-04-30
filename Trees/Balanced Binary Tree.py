9761733c41362ee4445805dd4054c2c13d40295a
osamasrour <oasmasror@gmail.com>
2026-04-30 03:49:00 +0300

    Solve 'Balanced Binary Tree' passed 13/23 test cases

---
+++ Trees/Balanced Binary Tree.py
@@ -0,0 +1,26 @@
+# Balanced Binary Tree: https://neetcode.io/problems/balanced-binary-tree/question?list=neetcode150
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        curr = root
+        if not curr: return True
+
+        left = self.trace(curr.left)
+        right = self.trace(curr.right)
+
+        return abs(left - right) <= 1
+
+    def trace(self, node):
+        if not node: return 0
+
+        left = self.trace(node.left)
+        right = self.trace(node.right)
+
+        return max(left, right) + 1
