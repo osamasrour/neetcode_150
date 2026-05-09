5f31f333d91c594e3b1471dbfc67be41f6d3948a
osamasrour <oasmasror@gmail.com>
2026-05-10 00:14:36 +0300

    Add TODO(#6)

--- Trees/Subtree of Another Tree.py
+++ Trees/Subtree of Another Tree.py
@@ -8,7 +8,7 @@
 #         self.right = right
 
 class Solution:
-    # TODO get rid of that attribute 'foundSub'
+    # TODO(#6): get rid of that attribute 'foundSub'
     foundSub = False
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if not p and not q : return True
